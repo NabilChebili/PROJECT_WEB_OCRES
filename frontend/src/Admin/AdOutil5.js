9e057b64a7b22e4a620059e15040f05725f2d7c2
NabilChebili <nabilchebili@ymail.com>
2020-11-29 01:08:58 +0100

    avancement storybook + admin

---
+++ frontend/src/Admin/AdOutil5.js
@@ -0,0 +1,235 @@
+import React from 'react';
+import './AdOutil5.css';
+
+class AdOutil5 extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+  
+        };
+    }
+  
+    
+  
+    render(){
+        return (           
+            <div class="">
+
+            <h5>Outil 5 : Epanouissement des salariés </h5>
+            <h7>Checker les mois dont vous voulez qu'on voit et entrez leurs valeurs</h7>
+            <br></br><br></br>
+            <form>
+                <label>
+                    <input class="checkbox" type="checkbox" id="lundi" name="lundi" checked></input>
+                    <text class="text">Janvier :</text>
+                    <br></br>
+                    <text class="text">18-25 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">25-45 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">45-65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">+65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                </label>
+                <br></br>
+                <label>
+                    <input class="checkbox" type="checkbox" id="mardi" name="mardi" checked></input>
+                    <text class="text">Février :</text>
+                    <br></br>
+                    <text class="text">18-25 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">25-45 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">45-65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">+65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                </label>
+                <br></br>
+                <label>
+                    <input class="checkbox" type="checkbox" id="mercredi" name="mercredi" checked></input> 
+                    <text class="text">Mars :</text>
+                    <br></br>
+                    <text class="text">18-25 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">25-45 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">45-65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">+65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                </label>
+                <br></br>
+                <label>
+                    <input class="checkbox" type="checkbox" id="jeudi" name="jeudi" checked></input>
+                    <text class="text">Avril :</text>
+                    <br></br>
+                    <text class="text">18-25 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">25-45 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">45-65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">+65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                </label>
+                <br></br>
+                <label>
+                    <input class="checkbox" type="checkbox" id="vendredi" name="vendredi" checked></input>
+                    <text class="text">Mai :</text>
+                    <br></br>
+                    <text class="text">18-25 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">25-45 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">45-65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">+65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                </label>
+                <br></br>
+                <label>
+                    <input class="checkbox" type="checkbox" id="samedi" name="samedi" checked></input>
+                    <text class="text">Juin :</text>
+                    <br></br>
+                    <text class="text">18-25 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">25-45 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">45-65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">+65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                </label>
+                <br></br>
+                <label>
+                    <input class="checkbox" type="checkbox" id="samedi" name="samedi" checked></input>
+                    <text class="text">Juillet :</text>
+                    <br></br>
+                    <text class="text">18-25 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">25-45 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">45-65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">+65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                </label>
+                <br></br>
+                <label>
+                    <input class="checkbox" type="checkbox" id="samedi" name="samedi" checked></input>
+                    <text class="text">Aout :</text>
+                    <br></br>
+                    <text class="text">18-25 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">25-45 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">45-65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">+65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                </label>
+                <br></br>
+                <label>
+                    <input class="checkbox" type="checkbox" id="samedi" name="samedi" checked></input>
+                    <text class="text">Septembre :</text>
+                    <br></br>
+                    <text class="text">18-25 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">25-45 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">45-65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">+65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                </label>
+                <br></br>
+                <label>
+                    <input class="checkbox" type="checkbox" id="samedi" name="samedi" checked></input>
+                    <text class="text">Octobre :</text>
+                    <br></br>
+                    <text class="text">18-25 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">25-45 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">45-65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">+65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                </label>
+                <br></br>
+                <label>
+                    <input class="checkbox" type="checkbox" id="samedi" name="samedi" checked></input>
+                    <text class="text">Novembre :</text>
+                    <br></br>
+                    <text class="text">18-25 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">25-45 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">45-65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">+65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                </label>
+                <br></br>
+                <label>
+                    <input class="checkbox" type="checkbox" id="samedi" name="samedi" checked></input>
+                    <text class="text">Décembre :</text>
+                    <br></br>
+                    <text class="text">18-25 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">25-45 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">45-65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                    <br></br>
+                    <text class="text">+65 :</text>
+                    <input class="checkbox" type="number" name="lundiNum"/>
+                </label>
+                <br></br>
+                <input type="submit" value="Envoyer" />
+            </form>
+
+            </div>
+        );
+    }
+}
+
+export default AdOutil5;
---
+++ frontend/src/stories/0-Page.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Page from '../Principale/App.js';
+
+export default {
+  title: 'Page',
+};
+
+export const Dashboard = () => <Page/>;
+
+
