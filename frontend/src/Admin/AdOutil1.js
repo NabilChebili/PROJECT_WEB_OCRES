2c651b767b72de72ef4273e49dcfb59ab190279f
NabilChebili <nabilchebili@ymail.com>
2020-12-12 18:07:34 +0100

    Avancement de outil1

--- frontend/src/Admin/AdOutil1.js
+++ frontend/src/Admin/AdOutil1.js
@@ -1,35 +1,219 @@
 import React from 'react';
 import './AdOutil1.css';
 
+import axios from 'axios';
+import { Component } from 'react';
+
+
+
 class AdOutil1 extends React.Component{
 
     constructor(props){
         super(props);
         this.state = {
-  
+            user: "Adrien",
+            data : [],
         };
+        
     }
   
-    
+    componentDidMount(){
+        var datatemp = [];
+        axios.get('http://localhost:3000/outil1')
+                .then((response) => 
+                {       
+                    var dataRecu;
+                    dataRecu = response.data.dataO1;
+                    for (let i = 0; i < dataRecu.length; i++) {
+                        if(dataRecu[i].visible === true){
+                            datatemp.push({jour : dataRecu[i].jour,taches : dataRecu[i].taches,visible : dataRecu[i].taches});
+                        }
+                    }
+                    this.setState({ data : dataRecu})
+                })
+    }
+
+    changeVisible(num){
+        if(this.state.data.length !== 0 )
+        {
+            let dataTemp = this.state.data;
+            dataTemp[num].visible = !this.state.data[num].visible
+            this.setState({data :  dataTemp})
+        }
+    }
+
+    changeNombre(num){
+        var mardi = document.getElementById("mardi").value;
+        if(this.state.data.length !== 0 )
+        {
+            let dataTemp = this.state.data;
+            dataTemp[num].taches = mardi
+            this.setState({data :  dataTemp})
+        }
+    }
+
+    Lundi(){
+        if(this.state.data.length == 0 )
+        {
+            return(
+                <label>
+                    <input class="checkbox" type="checkbox" id="lundi" name="lundi" checked></input>
+                    <text class="text">Lundi :</text>
+                    <input class="checkbox" type="number" name="lundiNum" />
+                </label>
+            )
+        }
+        else{
+            return(
+            <label>
+                <input class="checkbox" type="checkbox" id="lundi" name="lundi" checked></input>
+                <text class="text">Lundi :</text>
+                <input class="checkbox" type="number" name="lundiNum" value={this.state.data[0].taches} />
+            </label>
+            )
+        }
+
+       
+    }
+
+    Mardi(){
+        
+        let check = false;
+        let taches = 0;
+        
+        if(this.state.data.length !== 0 )
+        {
+            taches = this.state.data[1].taches;
+            if(this.state.data[1].visible === true)
+            {
+                check = true
+            }
+        }
+        
+        return(
+            <label>
+                <input class="checkbox" type="checkbox"  name="mardi" checked={check} onChange={() => this.changeVisible(1)} ></input>
+                <text class="text">Mardi :</text>
+                <input type="number" id="mardi" name="mardiNum" placeholder={taches} onChange={() => this.changeNombre(1)}/>
+            </label>
+        )
+        
+        
+
+       
+    }
+
+    Mercredi(){
+        if(this.state.data.length == 0 )
+        {
+            return(
+                <label>
+                    <input class="checkbox" type="checkbox" id="lundi" name="lundi" checked></input>
+                    <text class="text">Lundi :</text>
+                    <input class="checkbox" type="number" name="lundiNum" />
+                </label>
+            )
+        }
+        else{
+            return(
+            <label>
+                <input class="checkbox" type="checkbox" id="lundi" name="lundi" checked></input>
+                <text class="text">Lundi :</text>
+                <input class="checkbox" type="number" name="lundiNum" value={this.state.data[0].taches} />
+            </label>
+            )
+        }
+
+       
+    }
+
+    Jeudi(){
+        if(this.state.data.length == 0 )
+        {
+            return(
+                <label>
+                    <input class="checkbox" type="checkbox" id="lundi" name="lundi" checked></input>
+                    <text class="text">Lundi :</text>
+                    <input class="checkbox" type="number" name="lundiNum" />
+                </label>
+            )
+        }
+        else{
+            return(
+            <label>
+                <input class="checkbox" type="checkbox" id="lundi" name="lundi" checked></input>
+                <text class="text">Lundi :</text>
+                <input class="checkbox" type="number" name="lundiNum" value={this.state.data[0].taches} />
+            </label>
+            )
+        }
+
+       
+    }
+
+    Vendredi(){
+        if(this.state.data.length == 0 )
+        {
+            return(
+                <label>
+                    <input class="checkbox" type="checkbox" id="lundi" name="lundi" checked></input>
+                    <text class="text">Lundi :</text>
+                    <input class="checkbox" type="number" name="lundiNum" />
+                </label>
+            )
+        }
+        else{
+            return(
+            <label>
+                <input class="checkbox" type="checkbox" id="lundi" name="lundi" checked></input>
+                <text class="text">Lundi :</text>
+                <input class="checkbox" type="number" name="lundiNum" value={this.state.data[0].taches} />
+            </label>
+            )
+        }
+
+       
+    }
+
+    Samedi(){
+        if(this.state.data.length == 0 )
+        {
+            return(
+                <label>
+                    <input class="checkbox" type="checkbox" id="lundi" name="lundi" checked></input>
+                    <text class="text">Lundi :</text>
+                    <input class="checkbox" type="number" name="lundiNum" />
+                </label>
+            )
+        }
+        else{
+            return(
+            <label>
+                <input class="checkbox" type="checkbox" id="lundi" name="lundi" checked></input>
+                <text class="text">Lundi :</text>
+                <input class="checkbox" type="number" name="lundiNum" value={this.state.data[0].taches} />
+            </label>
+            )
+        }
+
+       
+    }
   
+
+    
+
     render(){
+        
+        
         return (           
-          <div class="">
+          <div>
               
               <h5>Outil 1 : Vos tâches éffectuées pendant la semaine </h5>
               <h7>Checker les jours dont vous voulez qu'on voit et entrez leurs valeurs</h7>
               <form>
-                  <label>
-                      <input class="checkbox" type="checkbox" id="lundi" name="lundi" checked></input>
-                      <text class="text">Lundi :</text>
-                      <input class="checkbox" type="number" name="lundiNum" />
-                  </label>
+                  {this.Lundi()}
                   <br></br>
-                  <label>
-                      <input class="checkbox" type="checkbox" id="mardi" name="mardi" checked></input>
-                      <text class="text">Mardi :</text>
-                      <input type="number" name="mardiNum" />
-                  </label>
+                  {this.Mardi()}
                   <br></br>
                   <label>
                       <input class="checkbox" type="checkbox" id="mercredi" name="mercredi" checked></input> 
@@ -61,6 +245,6 @@ class AdOutil1 extends React.Component{
           </div>
         );
     }
-  }
+}
 
 export default AdOutil1;
--- frontend/src/Principale/Outil1.js
+++ frontend/src/Principale/Outil1.js
@@ -4,22 +4,13 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis,ResponsiveContainer} from 
 
 import axios from 'axios';
 
-var dataRecu;
 
 
 
-const data = [];
 
-axios.get('http://localhost:3000/outil1')
-    .then((response) => 
-    {
-        dataRecu = response.data.dataO1;
-        for (let i = 0; i < dataRecu.length; i++) {
-            if(dataRecu[i].visible === true){
-            data.push({name : dataRecu[i].jour,uv : dataRecu[i].taches,pv: 3, amt: 2400});}
-        }
-        console.log(data);
-    })
+
+
+
     
 
 
@@ -33,19 +24,44 @@ class Outil1 extends React.Component{
         super(props);
         this.state = {
             user: "Adrien",
+            data: [],
         };
     }
 
+    componentDidMount(){
+        this.retourApi();
+    }
     
+    retourApi(){
+        try{
+            
+            axios.get('http://localhost:3000/outil1')
+           .then((response) => 
+           {
+            var data = [];    
+            var dataRecu;
+                dataRecu = response.data.dataO1;
+                for (let i = 0; i < dataRecu.length; i++) {
+                    if(dataRecu[i].visible === true){
+                    data.push({name : dataRecu[i].jour,uv : dataRecu[i].taches,pv: 3, amt: 2400});}
+                }
+                this.setState({data:data})
+               
+           })
+       }
+       catch(err){
+           console.log(err);
+       }
+    }
 
     render(){
-        console.log(data);
+        
         return (           
             <div>
                 <h6 class="texte">Vos tâches éffectuées pendant la semaine</h6>
                <div style={{ width: '95%', height: 300 }}>
                <ResponsiveContainer>
-                <LineChart width={500} height={300} data={data}>
+                <LineChart width={500} height={300} data={this.state.data}>
                     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
                     <CartesianGrid stroke="#ccc" />
                     <XAxis dataKey="name" />
